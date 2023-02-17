a4c7b5bd0d6d06e923171b8c85d25dc822fcbb91
Prabhaharan1392 <115686911+Prabhaharan1392@users.noreply.github.com>
2023-02-17 14:09:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+const starterPrice = document.getElementById("starter-price");
+const proPrice = document.getElementById("pro-price");
+const EnterpisePrice = document.getElementById("Enterpise-price");
